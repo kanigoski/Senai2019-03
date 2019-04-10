1d5adb8df61fad9c38ab1b0612f1167b9b971bfb
Richard Kanigoski <richardkanigoski1@gmail.com>
2019-04-09 21:08:18 -0300

    Added REST get

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Hello world!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true }));
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send({
+        "Teste": "olá Mundo"
+    });
+});
+app.listen(port, function () {
+    console.log("Example app listening on port " + port + "!");
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true }));
+
+const port: number = 3000;
+
+app.get('/', function (req, res) {
+    res.send({
+        "Teste": "olá Mundo"
+    });
+});
+
+app.listen(port, function () {
+console.log(`Example app listening on port ${port}!`);
+});
